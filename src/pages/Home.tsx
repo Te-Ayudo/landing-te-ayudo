681cb8adb78603b8491e51d2bd9c091ac8bb737e
Ilse Romero <ilseromero35@gmail.com>
2025-04-19 09:10:09 -0400

    refactor: remove PriceCards component from Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import Hero from "@/sections/Hero";
 import MissionVision from "@/sections/MissionVision";
 import Roadmap from "@/sections/Roadmap";
 import ProductPricing from "@/sections/ProductPricing";
-import PriceCards from "@/sections/PriceCards";
 import Partners from "@/sections/Partners";
 import Contact from "@/sections/Contact";
 
@@ -13,7 +12,6 @@ const Home = () => {
       <MissionVision />
       <Roadmap />
       <ProductPricing />
-      <PriceCards />
       <Partners />
       <Contact />
     </>
